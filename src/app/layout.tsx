5e4890021cb64344f6aec17a58114c7490840ad6
Vitor Tinelli <vitor.mtinelli@gmail.com>
2025-06-14 01:22:31 -0300

    feat/added-ddm-logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SGD | Policia DDM",
   description: "Sistema de gerenciamento de dados do Departamento de Desenvolvimento Militar",
+  icons: {
+    icon: "/ddmLogo.png",
+  },
 };
 
 export default function RootLayout({
